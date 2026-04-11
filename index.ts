1cb2e2cf6e727f8ecd299c37141d3fc5ee91c1d6
YoFi-0 <tomy.lamaBlama@gmail.com>
2026-04-11 18:22:22 +0300

    search for changes algo file
        const distanceToEntry = Math.abs(lastClose - sweepEntryPrice) / sweepEntryPrice;
        const isNearEntry = distanceToEntry <= preparationTolerance;

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ interface IConfig {
 export const config:IConfig = {
     symbol: 'SOL-USDT',
     leverage: 20,
-    mode: "bingx_test",
+    mode: "backtest",
     chartIntrval: "1m",
     sweepIntensity:2,
     usdtAmount: 50,
